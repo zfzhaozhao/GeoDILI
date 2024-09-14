682cc267ae490d3bd58033d3d4821d46edbec8a7
zhaozhao <152957335+zfzhaozhao@users.noreply.github.com>
2024-09-14 11:54:29 +0800

    Update inmemory_dataset.py

--- pahelix/datasets/inmemory_dataset.py
+++ pahelix/datasets/inmemory_dataset.py
@@ -42,8 +42,15 @@ class InMemoryDataset(object):
         The ``data_list`` can be cached in npz files by calling `dataset.save_data(data_path)` 
         and after that, call `InMemoryDataset(data_path)` to reload.
 
+
+InMemoryDataset 管理着一个 data_list，它是一个包含 data 的列表，而 data 是一个字典，字典中的值都是 numpy 的 ndarray。并且每个字典具有相同的键。
+
+它的工作方式类似于列表：你可以通过调用 dataset[i] 来获取 data_list 中的第 i 个元素，并通过调用 len(dataset) 来获取 data_list 的长度。
+
+data_list 可以通过调用 dataset.save_data(data_path) 缓存到 npz 文件中，之后可以通过调用 InMemoryDataset(data_path) 来重新加载。
+
     Attributes:
-        data_list(list): a list of dict of numpy ndarray.
+        data_list(list): a list of dict of numpy ndarray.  #传入的是字典（smiles:label)
 
     Example:
         .. code-block:: python
@@ -63,7 +70,7 @@ class InMemoryDataset(object):
         """
         Users can either directly pass the ``data_list`` or pass the `data_path` from 
         which the cached ``data_list`` will be loaded.
-
+#用户可以直接传递 data_list，或者传递 data_path，从中加载缓存的 data_list。
         Args:
             data_list(list): a list of dict of numpy ndarray.
             data_path(str): the path to the cached npz path.
@@ -99,6 +106,7 @@ class InMemoryDataset(object):
         n = len(data_list)
         for i in range(int((n - 1) / max_num_per_file) + 1):
             filename = 'part-%06d.npz' % i
+    # %06d 是格式化字符串，用于将 i 格式化为一个 6 位数的字符串，不足 6 位的部分用前导零填充。例如，i 为 3 时，文件名将为 part-000003.npz。
             sub_data_list = self.data_list[i * max_num_per_file: (i + 1) * max_num_per_file]
             save_data_list_to_npz(sub_data_list, join(data_path, filename))
 
@@ -112,7 +120,7 @@ class InMemoryDataset(object):
         """
         self._save_npz_data(self.data_list, data_path)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key):  #不是很明白，好像是key是切片，整数，列表，放回对应的数据
         if isinstance(key, slice):
             start, stop, step = key.indices(len(self))
             dataset = InMemoryDataset(
@@ -136,9 +144,9 @@ class InMemoryDataset(object):
         """
         Inplace apply `transform_fn` on the `data_list` with multiprocess.
         """
-        data_list = mp_pool_map(self.data_list, transform_fn, num_workers)
+        data_list = mp_pool_map(self.data_list, transform_fn, num_workers)  #mp_pool_map 函数的作用是利用多进程并行处理 self.data_list 中的每个数据项，以提高处理效率
         if drop_none:
-            self.data_list = [data for data in data_list if not data is None]
+            self.data_list = [data for data in data_list if not data is None]  #去掉None
         else:
             self.data_list = data_list
 
@@ -156,7 +164,7 @@ class InMemoryDataset(object):
                 multiprocess.
             shuffle(bool): whether to shuffle the order of the ``data_list``.
             collate_fn(function): used to convert the sub-list of ``data_list`` to the 
-                aggregated batch data.
+                aggregated batch data. #用于将data_list的子列表转换为聚合的批次数据。
 
         Yields:
             the batch data processed by ``collate_fn``.
@@ -167,3 +175,10 @@ class InMemoryDataset(object):
                 shuffle=shuffle,
                 drop_last=drop_last,
                 collate_fn=collate_fn)
+
+
+#drop_last 是一个布尔值，用于指定在数据集大小不能被批次大小整除时，是否丢弃最后一个不完整的批次。
+如果设置为 True，那么当最后一个批次不足 batch_size 时，将不会返回这个批次；如果 False，则会返回这个批次（可能大小小于 batch_size）。
+
+collate_fn 是一个可选的函数，用于定义如何将来自数据集的样本合并为一个批次。
+默认情况下，PyTorch 和 pgl 提供的 collate_fn 可以处理常见的数据类型，但如果数据结构更复杂，可以自定义这个函数来处理特定的情况。
