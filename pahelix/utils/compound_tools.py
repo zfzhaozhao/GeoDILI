43e8d550e6f799d8457599c53d70fbbf57502fff
zhaozhao <152957335+zfzhaozhao@users.noreply.github.com>
2024-09-16 23:44:18 +0800

    Update compound_tools.py

--- pahelix/utils/compound_tools.py
+++ pahelix/utils/compound_tools.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """
-| Tools for compound features.
+| Tools for compound features.  #化合物特征的提取，或者是用向量来表示分子
 | Adapted from https://github.com/snap-stanford/pretrain-gnns/blob/master/chem/loader.py
 """
 import os
@@ -27,25 +27,26 @@ from rdkit.Chem import rdchem
 from pahelix.utils.compound_constants import DAY_LIGHT_FG_SMARTS_LIST
 
 
-def get_gasteiger_partial_charges(mol, n_iter=12):
+def get_gasteiger_partial_charges(mol, n_iter=12): #用于获取原子电荷的函数
     """
     Calculates list of gasteiger partial charges for each atom in mol object.
 
     Args: 
-        mol: rdkit mol object.
+        mol: rdkit mol object.  #  传入的是mol对象
         n_iter(int): number of iterations. Default 12.
 
     Returns: 
-        list of computed partial charges for each atom.
+        list of computed partial charges for each atom. # 最后得到mol对象每一个原子的电荷
     """
     Chem.rdPartialCharges.ComputeGasteigerCharges(mol, nIter=n_iter,
-                                                  throwOnParamFailure=True)
+                                                  throwOnParamFailure=True)  #throwOnParamFailure=True: 这个参数控制如果计算过程中出现参数问题时是否抛出异常。如果设置为 True，遇到参数问题时会抛出异常；如果设置为 False，则不会抛出异常。
     partial_charges = [float(a.GetProp('_GasteigerCharge')) for a in
                        mol.GetAtoms()]
+    a.GetProp('_GasteigerCharge'): #这是 RDKit Atom 对象的方法，用于获取原子的 Gasteiger 部分电荷。'_GasteigerCharge' 是存储部分电荷的属性名。
     return partial_charges
 
 
-def create_standardized_mol_id(smiles):
+def create_standardized_mol_id(smiles): 
     """
     Args:
         smiles: smiles sequence.
@@ -56,7 +57,7 @@ def create_standardized_mol_id(smiles):
     if check_smiles_validity(smiles):
         # remove stereochemistry
         smiles = AllChem.MolToSmiles(AllChem.MolFromSmiles(smiles),
-                                     isomericSmiles=False)
+                                     isomericSmiles=False)  #不考虑立体化学
         mol = AllChem.MolFromSmiles(smiles)
         if not mol is None: # to catch weird issue with O=C1O[al]2oc(=O)c3ccc(cn3)c3ccccc3c3cccc(c3)c3ccccc3c3cc(C(F)(F)F)c(cc3o2)-c2ccccc2-c2cccc(c2)-c2ccccc2-c2cccnc21
             if '.' in smiles: # if multiple species, pick largest molecule
@@ -64,7 +65,7 @@ def create_standardized_mol_id(smiles):
                 largest_mol = get_largest_mol(mol_species_list)
                 inchi = AllChem.MolToInchi(largest_mol)
             else:
-                inchi = AllChem.MolToInchi(mol)
+                inchi = AllChem.MolToInchi(mol) #MolToInchi: 这是 AllChem 模块中的一个函数，用于将 RDKit 的分子对象转换为 InChI 标识符。InChI（International Chemical Identifier）是一个国际化学标识符，用于唯一标识化学物质。
             return inchi
         else:
             return
@@ -100,7 +101,7 @@ def split_rdkit_mol_obj(mol):
     for s in smiles_list:
         if check_smiles_validity(s):
             mol_species_list.append(AllChem.MolFromSmiles(s))
-    return mol_species_list
+    return mol_species_list  #是mol对象列表
 
 
 def get_largest_mol(mol_list):
@@ -116,21 +117,22 @@ def get_largest_mol(mol_list):
         the largest mol.
     """
     num_atoms_list = [len(m.GetAtoms()) for m in mol_list]
-    largest_mol_idx = num_atoms_list.index(max(num_atoms_list))
+    largest_mol_idx = num_atoms_list.index(max(num_atoms_list))#（num_atoms_list列表的最大值在num_atoms_list中的对应索引（第一个出现的）
     return mol_list[largest_mol_idx]
 
-def rdchem_enum_to_list(values):
-    """values = {0: rdkit.Chem.rdchem.ChiralType.CHI_UNSPECIFIED, 
-            1: rdkit.Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW, 
-            2: rdkit.Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW, 
-            3: rdkit.Chem.rdchem.ChiralType.CHI_OTHER}
+def rdchem_enum_to_list(values): #立体化学类型
+    """values = {0: rdkit.Chem.rdchem.ChiralType.CHI_UNSPECIFIED, #该原子的手性状态没有被明确指定或不适用。
+            1: rdkit.Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW, #代表四面体型（tetrahedral）手性中心，具有顺时针
+            2: rdkit.Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW, #代表四面体型手性中心，具有逆时针
+            3: rdkit.Chem.rdchem.ChiralType.CHI_OTHER}  #代其他复杂的手性配置类型。
     """
-    return [values[i] for i in range(len(values))]
+    return [values[i] for i in range(len(values))]  #获得是他们的字符串列表？（具体看传的参数长相咯）
 
 
 def safe_index(alist, elem):
     """
     Return index of element e in list l. If e is not present, return the last index
+    返回元素e在列表 中的索引，若没有在列表中找道该元素，则赋其索引为最后一个
     """
     try:
         return alist.index(elem)
@@ -138,7 +140,8 @@ def safe_index(alist, elem):
         return len(alist) - 1
 
 
-def get_atom_feature_dims(list_acquired_feature_names):
+def get_atom_feature_dims(list_acquired_feature_names):#传入的参数应该是各种特征的名字列表（是个字符串列表）  获取的是每个特征的具体维度（有点困惑，我会认为这个维度应该是一样的啊）
+    可能在这个方式中，每个特征的维度是不一样的，最后会处理吗？？？
     """ tbd
     """
     return list(map(len, [CompoundKit.atom_vocab_dict[name] for name in list_acquired_feature_names]))
@@ -149,12 +152,12 @@ def get_bond_feature_dims(list_acquired_feature_names):
     """
     list_bond_feat_dim = list(map(len, [CompoundKit.bond_vocab_dict[name] for name in list_acquired_feature_names]))
     # +1 for self loop edges
-    return [_l + 1 for _l in list_bond_feat_dim]
+    return [_l + 1 for _l in list_bond_feat_dim]  #考虑了自环的边，自己在总维度上+1
 
 
 class CompoundKit(object):
     """
-    CompoundKit
+    CompoundKit  #化合物具体的特征设置  在这儿的维度是不一样的
     """
     atom_vocab_dict = {
         "atomic_num": list(range(1, 119)) + ['misc'],
@@ -166,7 +169,7 @@ class CompoundKit(object):
         "implicit_valence": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 'misc'],
         "is_aromatic": [0, 1],
         "total_numHs": [0, 1, 2, 3, 4, 5, 6, 7, 8, 'misc'],
-        'num_radical_e': [0, 1, 2, 3, 4, 'misc'],
+        'num_radical_e': [0, 1, 2, 3, 4, 'misc'], #'num_radical_e'  #通常指的是分子中存在的自由基电子的数量
         'atom_is_in_ring': [0, 1],
         'valence_out_shell': [0, 1, 2, 3, 4, 5, 6, 7, 8, 'misc'],
         'in_num_ring_with_size3': [0, 1, 2, 3, 4, 5, 6, 7, 8, 'misc'],
@@ -177,12 +180,12 @@ class CompoundKit(object):
         'in_num_ring_with_size8': [0, 1, 2, 3, 4, 5, 6, 7, 8, 'misc'],
     }
     bond_vocab_dict = {
-        "bond_dir": rdchem_enum_to_list(rdchem.BondDir.values),
+        "bond_dir": rdchem_enum_to_list(rdchem.BondDir.values), #键的方向
         "bond_type": rdchem_enum_to_list(rdchem.BondType.values),
         "is_in_ring": [0, 1],
 
-        'bond_stereo': rdchem_enum_to_list(rdchem.BondStereo.values),
-        'is_conjugated': [0, 1],
+        'bond_stereo': rdchem_enum_to_list(rdchem.BondStereo.values), #用于表示化学键的立体化学（立体化学指的是分子中键的空间排布方式）。
+        'is_conjugated': [0, 1],  #用于表示化学键是否是共轭的。
     }
     # float features
     atom_float_names = ["van_der_waals_radis", "partial_charge", 'mass']
@@ -191,20 +194,23 @@ class CompoundKit(object):
     ### functional groups
     day_light_fg_smarts_list = DAY_LIGHT_FG_SMARTS_LIST
     day_light_fg_mo_list = [Chem.MolFromSmarts(smarts) for smarts in day_light_fg_smarts_list]
-
+    
+#从一个包含 SMARTS 模式字符串的列表 DAY_LIGHT_FG_SMARTS_LIST 中创建一个新的列表 day_light_fg_smarts_list。
+#使用 RDKit 的 Chem.MolFromSmarts 函数将 day_light_fg_smarts_list 中的每个 SMARTS 模式转换为 RDKit 的分子对象，并将这些分子对象存储在 day_light_fg_mo_list 列表中
+    
     morgan_fp_N = 200
     morgan2048_fp_N = 2048
     maccs_fp_N = 167
 
-    period_table = Chem.GetPeriodicTable()
+    period_table = Chem.GetPeriodicTable() #获取了 RDKit 的周期表对象，这个对象提供了对周期表中所有元素的访问能力，用于化学计算和分子建模中。
 
     ### atom
 
     @staticmethod
-    def get_atom_value(atom, name):
+    def get_atom_value(atom, name): #name  是特征的字符串名称
         """get atom values"""
         if name == 'atomic_num':
-            return atom.GetAtomicNum()
+            return atom.GetAtomicNum()  #这里是用原子序号表示原子类型哦
         elif name == 'chiral_tag':
             return atom.GetChiralTag()
         elif name == 'degree':
@@ -228,7 +234,8 @@ class CompoundKit(object):
         elif name == 'atom_is_in_ring':
             return int(atom.IsInRing())
         elif name == 'valence_out_shell':
-            return CompoundKit.period_table.GetNOuterElecs(atom.GetAtomicNum())
+            return CompoundKit.period_table.GetNOuterElecs(atom.GetAtomicNum()) #GetNOuterElecs 方法用于获取一个元素的外层电子数。外层电子是原子中位于最外层的电子，
+            #GetAtomicNum 方法会返回这个原子的原子序数。原子序数是一个整数，表示元素在周期表中的位置，也等于原子的质子数
         else:
             raise ValueError(name)
 
@@ -258,8 +265,8 @@ class CompoundKit(object):
         elif name == 'is_conjugated':
             return int(bond.GetIsConjugated())
         elif name == 'bond_stereo':
-            return bond.GetStereo()
-        else:
+            return bond.GetStereo()  #用于获取化学键的立体化学信息
+        else: 
             raise ValueError(name)
 
     @staticmethod
@@ -280,8 +287,10 @@ class CompoundKit(object):
     def get_morgan_fingerprint(mol, radius=2):
         """get morgan fingerprint"""
         nBits = CompoundKit.morgan_fp_N
-        mfp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=nBits)
+        mfp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=nBits) #nBits=nBits: 这是指纹向量的长度，即指纹的维度。nBits 是一个整数，指定了位向量中位的数量。
+        #较大的 nBits 值可以提供更高的分辨率，但也会增加计算和存储的需求，指定了nBits即每个分字的该指纹维度是一样的
         return [int(b) for b in mfp.ToBitString()]
+        #ToBitString(): 这是 ExplicitBitVect 对象的一个方法，将位向量转换为一个二进制字符串，其中每一位（bit）表示为 ‘0’ 或 ‘1’。
     
     @staticmethod
     def get_morgan2048_fingerprint(mol, radius=2):
@@ -293,7 +302,7 @@ class CompoundKit(object):
     @staticmethod
     def get_maccs_fingerprint(mol):
         """get maccs fingerprint"""
-        fp = AllChem.GetMACCSKeysFingerprint(mol)
+        fp = AllChem.GetMACCSKeysFingerprint(mol)  #这个方法的维度是确定，因为它内部的子结构是确定的大概是160多个，就是比较传入的分子有没有这些子结构
         return [int(b) for b in fp.ToBitString()]
 
     ### functional groups
@@ -306,11 +315,21 @@ class CompoundKit(object):
             sub_structs = Chem.Mol.GetSubstructMatches(mol, fg_mol, uniquify=True)
             fg_counts.append(len(sub_structs))
         return fg_counts
+#Chem.Mol.GetSubstructMatches(mol, fg_mol, uniquify=True): 这是 RDKit 中 Mol 对象的一个方法，用于查找目标分子 mol 中与功能基 fg_mol 结构匹配的子结构。
+
+mol: 这是你要在其中查找子结构的分子，应该是一个 RDKit 的 Mol 对象。
+fg_mol: 这是要在目标分子中查找的功能基。
+uniquify=True: 这个参数确保返回的匹配结果是唯一的，即不重复的子结构匹配。
+sub_structs: 这是一个列表，包含了所有与 fg_mol 匹配的子结构的原子索引元组。每个元组表示 fg_mol 在 mol 中的一个匹配位置。
 
     @staticmethod
     def get_ring_size(mol):
         """return (N,6) list"""
-        rings = mol.GetRingInfo()
+        rings = mol.GetRingInfo() #是 RDKit 中用来获取分子环信息的方法。它为你提供了有关分子中环的详细信息
+GetRingInfo(): 获取分子的 RingInfo 对象。
+AtomRings(): 返回苯分子中所有环的原子索引。
+BondRings(): 返回苯分子中所有环的键索引。
+NumRings(): 返回苯分子中环的数量（苯有一个六元环）
         rings_info = []
         for r in rings.AtomRings():
             rings_info.append(r)
@@ -365,7 +384,7 @@ class CompoundKit(object):
         ring_list = CompoundKit.get_ring_size(mol)
         for i, atom in enumerate(mol.GetAtoms()):
             atom_features_dicts[i]['in_num_ring_with_size3'] = safe_index(
-                    CompoundKit.atom_vocab_dict['in_num_ring_with_size3'], ring_list[i][0])
+                    CompoundKit.atom_vocab_dict['in_num_ring_with_size3'], ring_list[i][0])#这里的[i]代表的是第i个原子，[0]表示的是环大小，即每个原子是否属于不用数量的环大小都要遍历，各自为一个维度
             atom_features_dicts[i]['in_num_ring_with_size4'] = safe_index(
                     CompoundKit.atom_vocab_dict['in_num_ring_with_size4'], ring_list[i][1])
             atom_features_dicts[i]['in_num_ring_with_size5'] = safe_index(
@@ -383,7 +402,7 @@ class CompoundKit(object):
     def check_partial_charge(atom):
         """tbd"""
         pc = atom.GetDoubleProp('_GasteigerCharge')
-        if pc != pc:
+        if pc != pc:#是为了排除None值，None不等于自身，也不等于0哦
             # unsupported atom, replace nan with 0
             pc = 0
         if pc == float('inf'):
@@ -395,7 +414,7 @@ class CompoundKit(object):
 class Compound3DKit(object):
     """the 3Dkit of Compound"""
     @staticmethod
-    def get_atom_poses(mol, conf):
+    def get_atom_poses(mol, conf):  #获取原子3维坐标
         """tbd"""
         atom_poses = []
         for i, atom in enumerate(mol.GetAtoms()):
@@ -410,9 +429,10 @@ class Compound3DKit(object):
         """the atoms of mol will be changed in some cases."""
         try:
             new_mol = Chem.AddHs(mol)
-            res = AllChem.EmbedMultipleConfs(new_mol, numConfs=numConfs)
+            res = AllChem.EmbedMultipleConfs(new_mol, numConfs=numConfs) #res = AllChem.EmbedMultipleConfs(new_mol, numConfs=numConfs) 是 RDKit 中用于生成分子多重构象的方法。
+            它的作用是为一个给定的分子生成多个可能的三维构象列表
             ### MMFF generates multiple conformations
-            res = AllChem.MMFFOptimizeMoleculeConfs(new_mol)
+            res = AllChem.MMFFOptimizeMoleculeConfs(new_mol) #res: 这是 MMFFOptimizeMoleculeConfs 函数的返回值。res 是一个包含每个构象优化结果的字典，其中包括优化的状态信息和其他相关数据。
             new_mol = Chem.RemoveHs(new_mol)
             index = np.argmin([x[1] for x in res])
             energy = res[index][1]
