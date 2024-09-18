cebe405e6b53291a23d14aeff450ad6c2bd5f348
zhaozhao <152957335+zfzhaozhao@users.noreply.github.com>
2024-09-18 12:04:28 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -14,28 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-gnn network
+gnn network  #构建图神经网络模块，是下游任务的网络
 """
 
 import paddle
 import paddle.nn as nn
 import pgl
 
-from pahelix.networks.basic_block import MLP, Residual
+from pahelix.networks.basic_block import MLP, Residual  #使用了残差网络
 
 
 class DownstreamModel(nn.Layer):
     """
     Docstring for DownstreamModel,it is an supervised 
     GNN model which predicts the tasks shown in num_tasks and so on.
+    #这是一个监督学习的图神经网络（GNN）模型，用于预测在 num_tasks 中显示的任务等。
     """
 
     def __init__(self, model_config, compound_encoder):
         super(DownstreamModel, self).__init__()
         self.task_type = model_config['task_type']
         self.num_tasks = model_config['num_tasks']
-
+#model_config 是一个用来记录了在模型中要用到的配置，和超参数
+        
         self.compound_encoder = compound_encoder
+        #这个嵌入层是是传入的函数，得关注一下
         self.norm = nn.LayerNorm(compound_encoder.graph_dim)
         self.hid = 256
         # self.mlp = MLP(
